ed04f79510e3fb498fbabe38f0221a32eb48443a
Tuomo Kankaanpää <tuomo@tuomokankaanpaa.com>
2022-07-26 10:57:03 +0300

    Testing new middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server'
 
-export async function middleware(req) {
-  const { nextUrl: url, geo } = req
-  url.searchParams.set('country', geo.country)
-  return NextResponse.rewrite(url)
+// export async function middleware(req) {
+//   const { nextUrl: url, geo } = req
+//   url.searchParams.set('country', geo.country)
+//   return NextResponse.rewrite(url)
+// }
+
+export function middleware(request) {
+  if (!request.nextUrl.pathname.startsWith('/api/hello')) {
+    return NextResponse.redirect(new URL('/api/hello', request.url))
+  }
 }
+
